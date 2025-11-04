ede7ece6755a53fd166f6ed197608e574e26df49
Rebekkah LaBlue <lablue.rebekkah@gmail.com>
2025-11-04 16:30:13 -0600

    Syntax edits to naming convedntions of dfs.

--- scripts/4d_EffortCovariates.R
+++ scripts/4d_EffortCovariates.R
@@ -25,7 +25,7 @@ library(purrr)
 # "colonization" since high effort means lowers probability of true non-detection.
 
 # Put raw SR into the modeling df
-wibba_modeling_covars <- wibba_modeling_covars %>%
+wibba_covars_raw <- wibba_covars_raw %>%
   left_join(
     sr_summary_comp %>%
       dplyr::select(atlas_block, sr_Atlas1, sr_Atlas2),
@@ -34,11 +34,11 @@ wibba_modeling_covars <- wibba_modeling_covars %>%
 
 
 # Extract relevant covars for residual regression, z-scale ################### Note: Refine this list? Also how to scale? (global vs response-subset block data)
-covars_for_sr <- wibba_modeling_covars %>%
+covars_for_sr <- wibba_covars_raw %>%
   dplyr::select(
     atlas_block,
-    pa_percent, water_open_2008, shrub_scrub_2008, grassland_2008,
-    developed_total_2008, forest_total_2008, wetlands_total_2008,
+    pa_percent, water_open_base, shrub_scrub_base, grassland_base,
+    developed_total_base, forest_total_base, wetlands_total_base,
     lat, lon
   )
 
@@ -51,17 +51,18 @@ covars_for_sr <- covars_for_sr %>%
     )
   )
 
-covars_for_sr <- covars_for_SR %>%
+covars_for_sr <- covars_for_sr %>%
   left_join(
   sr_summary_comp %>%
     dplyr::select(atlas_block, sr_Atlas1, sr_Atlas2),
   by = "atlas_block"
-)
+  ) %>%
+  dplyr::select(-c(2:10))
 
-# Fit models on un-standardized SR
+# Fit models on un-standardized SR; logistic mod
 # Use grouped land cover covariates, climate proxy covariates for ease
-mod_srA1 <- lm(sr_Atlas1 ~ pa_percent_z + water_open_2008_z + shrub_scrub_2008_z + grassland_2008_z + developed_total_2008_z + forest_total_2008_z + wetlands_total_2008_z + lat_z + lon_z, data = covars_for_sr)
-mod_srA2 <- lm(sr_Atlas2 ~ pa_percent_z + water_open_2008_z + shrub_scrub_2008_z + grassland_2008_z + developed_total_2008_z + forest_total_2008_z + wetlands_total_2008_z + lat_z + lon_z, data = covars_for_sr)
+mod_srA1 <- lm(sr_Atlas1 ~ pa_percent_z + water_open_base_z + shrub_scrub_base_z + grassland_base_z + developed_total_base_z + forest_total_base_z + wetlands_total_base_z + lat_z + lon_z, data = covars_for_sr)
+mod_srA2 <- lm(sr_Atlas2 ~ pa_percent_z + water_open_base_z + shrub_scrub_base_z + grassland_base_z + developed_total_base_z + forest_total_base_z + wetlands_total_base_z + lat_z + lon_z, data = covars_for_sr)
 
 # Extract residuals, add to summaries
 sr_resids <- covars_for_sr %>%
@@ -74,6 +75,6 @@ sr_resids <- covars_for_sr %>%
 sr_summary_comp <- sr_summary_comp %>%
   left_join(sr_resids, by = "atlas_block")
 
-wibba_modeling_covars <- wibba_modeling_covars %>%
+wibba_covars_raw <- wibba_covars_raw %>%
   left_join(sr_resids, by = "atlas_block") %>%
   dplyr::select(-sr_Atlas1, -sr_Atlas2)
